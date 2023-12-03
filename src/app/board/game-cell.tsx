1962252d8cd27d431e9b6029d906f9c8a7f142c8
huang <huang.manutea@gmail.com>
2023-12-02 14:40:08 -1000

    fixed tuto

--- src/app/board/game-cell.tsx
+++ src/app/board/game-cell.tsx
@@ -5,5 +5,5 @@ export type GameCellProps = {
 }
 
 export default function GameCell({ lineIndex, cellIndex, value }: GameCellProps) {
-  return <div id={`cell-${lineIndex}-${cellIndex}`} className="h-16 w-16 text-xl text-bold text-center border text-white flex flex-col items-center justify-center"><span>{value}</span></div>;
+  return <div id={`cell-${lineIndex}-${cellIndex}`} className="h-16 aspect-square text-xl text-bold text-center border text-white flex flex-col items-center justify-center"><span>{value}</span></div>;
 }
--- src/app/components/modal-tuto.tsx
+++ src/app/components/modal-tuto.tsx
@@ -9,32 +9,38 @@ export default function ModalTuto() {
         </form>
         <h3 className="font-bold text-lg">Comment jouer?</h3>
         <p className="py-2">Chaque jour, à partir de 8h00, vous devez deviner un mot aléatoire en tahitien, de 5 lettres.</p>
-        <p className="py-2">Vous pouvez taper avec votre clavier, ou utiliser le clavier virtuel adapté à la langue tahitienne, tapez la touche &quot;entrée&quot; pour valider une tentative.</p>
-        <p className="py-2">Au cas où, le &apos;eta est une consonne, et compte donc comme une lettre, le &apos;eta correspond à la touche &quot;apostrophe&quot; du clavier.</p>
+        <p className="py-2">Vous pouvez taper avec votre clavier, ou utiliser le clavier virtuel adapté à la langue tahitienne:</p>
+        <ul className="list-disc">
+          <li>La touche &quot;entrée&quot; valide une tentative</li>
+          <li>Le &apos;eta est une consonne, et compte donc comme une lettre, le &apos;eta correspond à la touche &quot;apostrophe&quot;</li>
+          <li>La touche &quot;⇧&quot; permet d'afficher les tārava, elle correspond à la touche &quot;Majuscule&quot; du clavier</li>
+        </ul>
+        
+        <p className="py-2"></p>
         <p className="py-2">Vous avez 6 tentatives au total, vous gagnez si vous trouvez le mot en 6 tentatives ou moins.</p>
         <p className="py-2">Une fois la tentative validée, les cases seront colorés tels que:</p>
         <ul className="flex flex-col gap-2">
           <li>
             <div className="flex flex-row items-center gap-2">
-              <div className="h-16 w-16 text-xl text-bold text-center border text-white flex flex-col items-center justify-center cell-correct"><span>A</span></div>
+              <div className="h-16 aspect-square text-xl text-bold text-center border text-white flex flex-col items-center justify-center cell-correct"><span>A</span></div>
               <span>: la lettre est correctement positionnée</span>
             </div>
           </li>
           <li>
             <div className="flex flex-row items-center gap-2">
-              <div className="h-16 w-16 text-xl text-bold text-center border text-white flex flex-col items-center justify-center cell-misplaced"><span>A</span></div>
+              <div className="h-16 aspect-square text-xl text-bold text-center border text-white flex flex-col items-center justify-center cell-misplaced"><span>A</span></div>
               <span>: la lettre est mal positionnée</span>
             </div>
           </li>
           <li>
             <div className="flex flex-row items-center gap-2">
-              <div className="h-16 w-16 text-xl text-bold text-center border text-white flex flex-col items-center justify-center"><span>A</span></div>
+              <div className="h-16 aspect-square text-xl text-bold text-center border text-white flex flex-col items-center justify-center"><span>A</span></div>
               <span>: la lettre n&apos;existe pas dans le mot</span>
             </div>
           </li>
         </ul>
         <p className="py-2">Les points sont calculés de la façon suivante:</p>
-        <ul>
+        <ul className="list-disc">
           <li>Plus vous répondez vite après que le mot soit généré, plus vous marquez de points.</li>
           <li>Moins vous faites de tentatives pour trouver le mot, plus vous marquez de points.</li>
         </ul>
